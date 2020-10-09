9f05baaf2ede02b1714602b66ad8644d5221dddf
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-10-09 18:18:20 -0300

    Add status field in the ItemList component

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -1,16 +1,16 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
-import {reduxForm, Field, formValueSelector} from 'redux-form'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { reduxForm, Field, formValueSelector } from 'redux-form'
 
-import {init} from './billingCycleActions'
+import { init } from './billingCycleActions'
 import LabelAndInput from '../common/form/labelAndInput'
 import ItemList from './itemList'
 
-class BillingCycleForm extends Component{
-    render(){
-        const {handleSubmit, readOnly, credits} = this.props
-        return(
+class BillingCycleForm extends Component {
+    render() {
+        const { handleSubmit, readOnly, credits, debts } = this.props
+        return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
                     <Field name='name' component={LabelAndInput} readOnly={readOnly}
@@ -21,6 +21,8 @@ class BillingCycleForm extends Component{
                         type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
                     <ItemList cols='12 6' list={credits} readOnly={readOnly}
                         field='credits' legend='Créditos' />
+                    <ItemList cols='12 6' list={debts} readOnly={readOnly}
+                        field='debts' legend='Débitos' showStatus={true} />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className={`btn btn-${this.props.submitClass}`}>
@@ -34,9 +36,12 @@ class BillingCycleForm extends Component{
     }
 }
 
-BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
 const selector = formValueSelector('billingCycleForm')
 
-const mapStateToProps = state => ({credits: selector(state, 'credits')})
-const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
+const mapStateToProps = state => ({
+    credits: selector(state, 'credits'),
+    debts: selector(state, 'debts')
+})
+const mapDispatchToProps = dispatch => bindActionCreators({ init }, dispatch)
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleForm)
