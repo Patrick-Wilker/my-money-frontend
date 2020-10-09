6ceb79c7dd60efa8f0d2c40942283cc9305d618e
Patrick Wilker Vieira Mota <patrickvieiramota@gmail.com>
2020-10-09 18:49:55 -0300

    Integrate Sum of Credits and Debits

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -6,6 +6,7 @@ import {getSummary} from './DashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends Component {
 
@@ -19,12 +20,15 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <ValueBox cols='12 4' color='green' icon='bank'
-                        value={`R$ ${credit}`} text='Total de Créditos' />
-                    <ValueBox cols='12 4' color='red' icon='credit-card'
-                        value={`R$ ${debt}`} text='Total de Débitos' />
-                    <ValueBox cols='12 4' color='blue' icon='money'
-                        value={`R$ ${credit - debt}`} text='Valor Consolidado' />
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value={`R$ ${credit}`} text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value={`R$ ${debt}`} text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value={`R$ ${credit - debt}`} text='Valor Consolidado' />
+
+                    </Row>
 
                 </Content>
             </div>
